e9a0b2db7246b85095bebd1145c53be4c55127f6
Jean Michael <jeanmmclaudino@gmail.com>
2022-08-10 21:55:48 -0300

    Add project files.

---
+++ BaltaDataAccess/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace BaltaDataAccess
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server = LOCALHOST; Database = balta; Trusted_Connection = True";
+            
+            using(var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                // INSERT
+                // UPDATE
+                connection.Close();
+            }
+
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
