5a523a35e8f9299a91f3c9532ece7fbfcf5523cb
Jean Michael <jeanmmclaudino@gmail.com>
2022-08-14 16:15:02 -0300

    dapper

--- BaltaDataAccess/Program.cs
+++ BaltaDataAccess/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.Data.SqlClient;
+using Dapper;
+using BaltaDataAccess.Model;
 
 namespace BaltaDataAccess
 {
@@ -10,11 +12,35 @@ namespace BaltaDataAccess
         {
             const string connectionString = "Server = LOCALHOST; Database = balta; Trusted_Connection = True";
             
+            var list = new List<Category>();
+
             using(var connection = new SqlConnection(connectionString))
             {
-                connection.Open();
-                // INSERT
-                // UPDATE
+                //connection.Open();
+
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+
+                foreach(var category in categories)
+                {
+                    Console.WriteLine($"{category.Id} - {category.Title}");
+                }
+
+
+
+
+
+                //using (var command = new SqlCommand())
+                //{
+                //    command.Connection = connection;
+                //    command.CommandType = System.Data.CommandType.Text;
+                //    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                //    var reader = new command.ExecuteReader();
+                //    while (reader.Read())
+                //    {
+                //        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                //    }
+                //}
                 connection.Close();
             }
 
